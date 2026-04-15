f5faa916cb4fed049740e03b66d11f5f84f4f8bf
Alex <adiospallicer@cifpfbmoll.eu>
2026-04-15 17:31:26 +0200

    Actividad 3

--- Triggers/Act 1.sql
+++ Triggers/Act 1.sql
@@ -10,3 +10,30 @@
     end;
 
 INSERT INTO ANIMALES VALUES ('viejito','León',25,200,200,'El gran carnivoro','La grande')
+
+-- 3 TRES SITUACIONES PARA ACTUALIZAR GANANCIAS
+-- SE HACE UN SHOW DE UNA ATRACCIÓN
+create trigger aft_ins_show after insert on ATRACCION_DIA for each row
+    begin
+        update ATRACCIONES a
+        set a.ganancias = a.ganancias+NEW.ganancias
+        where a.nombre = NEW.nombre_atraccion;
+    end;
+
+insert into ATRACCION_DIA values ('El gran felino',now(),100,10000);
+-- SE QUITA UN SHOW (con un delete)
+create trigger  aft_del_show after delete on ATRACCION_DIA for each row
+    begin
+        update ATRACCIONES a
+        set a.ganancias=a.ganancias-OLD.ganancias
+        where a.nombre = OLD.nombre_atraccion;
+    end;
+
+delete from ATRACCION_DIA where nombre_atraccion = 'El gran felino' and fecha=date(now());
+-- SE MODIFICA GANANCIAS DE UN SHOW (con update)
+create trigger aft_upd_show after update on ATRACCION_DIA for each row
+    begin
+        update ATRACCIONES a
+        set a.ganancias=a.ganancias+(NEW.ganancias-OLD.ganancias)
+        where a.nombre = OLD.nombre_atraccion;
+    end;
